e32cf9f1946eae0d2317c255b4fba95ed8a0b9d6
Ayub Onyiego <onyiegoayub@gmail.com>
2024-07-26 02:38:24 +0300

    update moving card testimonials

--- app/snippets/infinite-moving-card-snippet.tsx
+++ app/snippets/infinite-moving-card-snippet.tsx
@@ -18,31 +18,31 @@ export function InfiniteMovingCardsDemo() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+      "Outstanding grasp of requirements and professional delivery. A pleasure to work with Yubitech!",
+    name: "Silk Route Advisors",
+    title: "Consulting Company India",
   },
   {
     quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
+      "Yubitech's professionalism and punctuality in meeting project requirements was exceptional.",
+    name: "William S",
+    title: "Gym Instructor, Berlin",
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+    quote: "Yubitech's perfect job, efficient coding, and superb communication made a huge difference. Can't wait for the next project!",
+    name: "Edgar",
+    title: "Project Manager",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+      "Demonstrated exceptional delivery quality, with a commendable commitment to resolving issues thoroughly until project completion. My gratitude for their dedicated service.",
+    name: "Vincent",
+    title: "USA",
+  },
+  {
+    quote:
+      "Yubitech showcases not only exceptional expertise in their field but also a remarkable ease of communication. We would definitely consider engaging their services again for future projects. Highly recommended for their professionalism and skill!",
+    name: "Mark O",
+    title: "Kenya",
   },
 ];
